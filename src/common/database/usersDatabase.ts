14a3fa0e6be53b451940fe9bde740b7890b96256
valera <traberker901@gmail.com>
2022-11-29 15:40:29 +0300

    features: add userModel

--- src/common/database/usersDatabase.ts
+++ src/common/database/usersDatabase.ts
@@ -2,17 +2,25 @@ import { connect } from "http2"
 import { MongoClient } from "mongodb"
 import mongoose from "mongoose"
 export const mongoUri = process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost:27017"
-// const mongoUri = "mongodb://localhost:27017"
-console.log(mongoUri)
+
 export type UserType = {
   id: string
   userName: string
   token: string
 }
+
+export type UsersMessageType = {
+  id: string
+  textMessage: string
+  themeMessage: string
+  dateOfSend: Date
+}
 export const client = new MongoClient(mongoUri)
 
-const usersDb = client.db("users-chat")
+export const usersDb = client.db("users-chat")
 export const usersCollection = usersDb.collection<UserType>("users")
+export const messsagesCollection = usersDb.collection<UsersMessageType>("messages")
+
 export async function runDb() {
   try {
     await client.connect()
