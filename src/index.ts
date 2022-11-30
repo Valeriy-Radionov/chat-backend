e5ca7e6b522c10009179641fa8f5c7ecacb1827a
valera <traberker901@gmail.com>
2022-12-01 01:58:38 +0300

    bugFix: add message

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import cors from "cors"
+import EventEmitter from "events"
 import express from "express"
 import { runDb } from "./common/database/usersDatabase"
 import { authRouter } from "./routes/authRoutes"
 import { messagesRouter } from "./routes/messagesRoutes"
 import { usersRouter } from "./routes/usersRoutes"
+
 require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 5000
